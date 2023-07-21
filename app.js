6e7e0c4dcc73a15ad83acbc9a39fe2095d0f9e2e
Liya Hbtemariam <liyahabtemariam11@gmail.com>
2023-07-20 18:08:22 -0700

    db connection details ignored

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');  
 const app = express();
+const dotenv = require('dotenv');
 
 const mongoose = require('mongoose');
 const { result } = require('lodash');
 const noteRoutes = require('./routes/noteRoutes')
 
-const  dbURI = 'mongodb+srv://Liya:Liya1*habte@cluster0.t9ag3i7.mongodb.net/Notesdb?retryWrites=true&w=majority';
+dotenv.config({path: "./.env"})
+const connectionUrl = process.env.CONNECTION_URL;
+PORT = process.env.PORT; 
 
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-.then((result)=>{console.log('connected to a database'); app.listen(3000); console.log('listening on port 3000')})
+mongoose.connect(connectionUrl, {useNewUrlParser: true, useUnifiedTopology: true})
+.then((result)=>{console.log('connected to a database'); app.listen(PORT); console.log('listening on port 3000')})
 .catch((err)=>{console.log(err)});
 
 app.set('view engine', 'ejs'); 
@@ -19,6 +22,4 @@ app.use(express.urlencoded({extended: true}));
 //Routes
 app.use(noteRoutes)
 
-app.use((req,res)=>{
-res.status(404).render('404', {title:'404'}) 
-})
+
