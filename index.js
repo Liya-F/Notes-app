607c40d7e9a32eb6e001838b2ab48688371e79e8
Liya Habtemariam <liyahabtemariam11@gmail.com>
2023-07-22 04:49:37 -0700

    port modification

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ dotenv.config({path: "./.env"})
 const dbUsername = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
 const connectionUrl = "mongodb+srv://"+dbUsername+":"+dbPassword+"@cluster0.t9ag3i7.mongodb.net/Notesdb?retryWrites=true&w=majority",
-PORT = process.env.PORT; 
+PORT = process.env.PORT || 3000; 
 
 mongoose.connect(connectionUrl, {useNewUrlParser: true, useUnifiedTopology: true})
-.then((result)=>{console.log('connected to a database'); app.listen(PORT); console.log('listening on port 3000')})
+.then((result)=>{console.log('connected to a database'); app.listen(PORT, '0.0.0.0'); console.log('listening on port ' + PORT)})
 .catch((err)=>{console.log(err)});
 
 app.set('view engine', 'ejs'); 
